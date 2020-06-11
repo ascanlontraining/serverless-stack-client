e1d6c50951a36277793c37eae5ea548b55493e44
andrew <andrewjscanlon@yahoo.co.uk>
2020-06-11 14:00:26 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+            REGION: "eu-west-2",
+            BUCKET: "ajs-notes-app-uploads"
+        },
+    apiGateway: {
+            REGION: "eu-west-2",
+            URL: "https://6fui64y5q8.execute-api.eu-west-2.amazonaws.com/prod"
+        },
+    cognito: {
+            REGION: "eu-west-2",
+            USER_POOL_ID: "eu-west-2_VtBexg4gn",
+            APP_CLIENT_ID: "7m8qoghqbdkjq5f0paoe7k68ur",
+            IDENTITY_POOL_ID: "eu-west-2:aa4c2766-6503-49d1-b1d0-891c62c2d6ba"
+            }
+    };
