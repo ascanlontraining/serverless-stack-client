2d9f1b2ef78ee812be5a0191e6620d299e7a367f
andrew <andrewjscanlon@yahoo.co.uk>
2020-06-18 16:05:36 +0100

    remove public stripe key

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const dev = {
-  STRIPE_KEY: pk_test_51GqyuADaaBQ1LA5TrwcR1fYAIOG5OVl0Q5PlhJD4Ah8jcveWZFskKKiw2dWnotQbIdwHMrASTkKeVeXb0tJcVx5H00vMnFngAL,
+  //STRIPE_KEY: pk_test_51GqyuADaaBQ1LA5TrwcR1fYAIOG5OVl0Q5PlhJD4Ah8jcveWZFskKKiw2dWnotQbIdwHMrASTkKeVeXb0tJcVx5H00vMnFngAL,
   s3: {
     REGION: "eu-west-2",
     BUCKET: "notes-app-2-api-dev-serverlessdeploymentbucket-oiyhmll6oo71"
@@ -17,7 +17,7 @@ const dev = {
 };
 
 const prod = {
-  STRIPE_KEY: pk_test_51GqyuADaaBQ1LA5TrwcR1fYAIOG5OVl0Q5PlhJD4Ah8jcveWZFskKKiw2dWnotQbIdwHMrASTkKeVeXb0tJcVx5H00vMnFngAL,
+  //STRIPE_KEY: pk_test_51GqyuADaaBQ1LA5TrwcR1fYAIOG5OVl0Q5PlhJD4Ah8jcveWZFskKKiw2dWnotQbIdwHMrASTkKeVeXb0tJcVx5H00vMnFngAL,
   s3: {
     REGION: "eu-west-2",
     BUCKET: "notes-app-2-api-prod-attachmentsbucket-1dduge969nrb0"
